0b0bbd6bc5c8cec6f1259a4abe910965e2456d1c
Ahmed Shaker <ashaker6573@gmail.com>
2023-11-01 17:03:27 +0200

    add onChange and onSubmit and  fix header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -41,6 +41,8 @@ export default function Header() {
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <button>
             <FaSearch className="text-slate-600" />
